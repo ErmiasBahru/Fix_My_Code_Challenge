3a2fd6e2f287e8723089116a8c11f68e261fe703
Ermias Bahru <ermiasbahru878@gmail.com>
2022-10-11 13:21:00 -0400

    square

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+class Square():
+
+    def __init__(self, *args, **kwargs):
+        self.width = 0
+        self.height = 0
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def permiter_Of_my_square(self):
+        """ Perimeter of the square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ String representation of the square """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_Of_my_square())
