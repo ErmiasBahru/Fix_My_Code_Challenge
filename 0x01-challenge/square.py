751fc489e4601e35cfa52b5e87ecb0b940d8640c
Ermias Bahru <ermiasbahru878@gmail.com>
2022-10-11 13:29:12 -0400

    comment added

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
+"""Class Square
+Defines a squaare geometry
+"""
 
-class Square():
+
+class Square:
+    """
+    Defines square geometry
+    """
 
     def __init__(self, *args, **kwargs):
+        """Initializes the Square class
+        Args:
+            args: arguments passed on instance
+            kwargs: keyword arguments passes on instance
+        Attributes:
+            width (int): the width of the square
+            height (int): the height of the square
+        """
         self.width = 0
         self.height = 0
         for key, value in kwargs.items():
@@ -10,9 +25,9 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def permiter_Of_my_square(self):
+    def permiter_of_my_square(self):
         """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
@@ -25,4 +40,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.permiter_Of_my_square())
+    print(s.permiter_of_my_square())
